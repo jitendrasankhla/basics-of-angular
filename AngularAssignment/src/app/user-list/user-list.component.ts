d90a80eb3c9e8d86853b4cb1b271d30c73bd28c9
Jitendra Sankhla <jitendra.sankhla@metacube.com>
2020-08-27 09:34:13 +0530

    Added show button

--- AngularAssignment/src/app/user-list/user-list.component.ts
+++ AngularAssignment/src/app/user-list/user-list.component.ts
@@ -8,17 +8,21 @@ import { User } from 'src/models/user';
 })
 export class UserListComponent implements OnInit {
 
-  users = [
+  users:any = [
     new User('Joey', 'Anderson', 'j.andrsn@gmail.com', true),
-    new User('mat', 'Pattrick', 'm.pat@gmail.com', true),
+    new User('Mat', 'Pattrick', 'm.pat@gmail.com', true),
     new User('Ron', 'Gelly', 'r.gel@gmail.com', true),
     new User('Rach', 'Green', 'r.green@gmail.com', true),
     new User('Phill', 'Richard', 'p.rich@gmail.com', true)
   ];
 
+  public selectedUser: User; 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onClick(user:any) {
+    this.selectedUser = user;
+  }
 }
